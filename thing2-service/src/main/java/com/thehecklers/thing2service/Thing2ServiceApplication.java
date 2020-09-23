1431563edb6d34b7aec983b2699dac912668e044
Mark Heckler <mark.heckler@gmail.com>
2020-09-23 07:49:41 -0500

    Initial commit to WS, WIP

--- thing2-service/src/main/java/com/thehecklers/thing2service/Thing2ServiceApplication.java
+++ thing2-service/src/main/java/com/thehecklers/thing2service/Thing2ServiceApplication.java
@@ -5,10 +5,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -18,6 +30,74 @@ public class Thing2ServiceApplication {
         SpringApplication.run(Thing2ServiceApplication.class, args);
     }
 
+}
+
+@Component
+class WebSocketHandler extends TextWebSocketHandler {
+    private final List<WebSocketSession> sessionList = new ArrayList<>();
+
+//    public List<WebSocketSession> getSessionList() {
+//        return sessionList;
+//    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessionList.add(session);
+        System.out.println("Connection established from " + session.toString() + " @ " + Instant.now().toString());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        try {
+            System.out.println("Message received: '" + message + "', from " + session.toString());
+
+            for (WebSocketSession sessionInList : sessionList) {
+                if (sessionInList != session) {
+                    sessionInList.sendMessage(message);
+                    System.out.println("--> Sending message '" + message + "' to " + sessionInList.toString());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception handling message: " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        //super.handleMessage(session, message);
+        if (message instanceof Thing1) {
+            for (WebSocketSession sessionInList : sessionList) {
+                if (sessionInList != session) {
+                    sessionInList.sendMessage(new TextMessage(new Thing2(UUID.randomUUID().toString(), "WS Thing2").toString()));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessionList.remove(session);
+        System.out.println("Connection closed by " + session.toString() + " @ " + Instant.now().toString());
+    }
+}
+
+@EnableWebSocket
+class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler handler;
+
+    WebSocketConfig(WebSocketHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws");
+    }
+
+    /*@Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+
+    }*/
 }
 
 @RestController
