52650736c94e8f7883027709dc6a7bf146f6c995
Mark Heckler <mark.heckler@gmail.com>
2020-09-23 11:58:56 -0500

    Initial commit: Messaging

--- thing1-service/src/main/java/com/thehecklers/thing1service/Thing1ServiceApplication.java
+++ thing1-service/src/main/java/com/thehecklers/thing1service/Thing1ServiceApplication.java
@@ -1,29 +1,22 @@
 package com.thehecklers.thing1service;
 
-import io.rsocket.SocketAcceptor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import java.time.Duration;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class Thing1ServiceApplication {
@@ -34,93 +27,11 @@ public class Thing1ServiceApplication {
 
 }
 
-@Component
-class RSComponent {
-    private final RSocketRequester requester;
-
-    public RSComponent(RSocketRequester.Builder builder) {
-        this.requester = builder.connectTcp("localhost", 8091).block();
-    }
-
-    //    @PostConstruct
-    void oneToOne() {
-        requester.route("thing2")
-                .data(new Thing1(UUID.randomUUID().toString(), "RSocket Thing1"))
-                .retrieveMono(Thing2.class)
-                .subscribe(t2 -> System.out.println("   >>> T2: " + t2));
-    }
-
-    //	@PostConstruct
-    void oneToManyFinite() {
-        requester.route("afewthing2s")
-                .data(new Thing1(UUID.randomUUID().toString(), "RSocket Thing1"))
-                .retrieveFlux(Thing2.class)
-                .subscribe(t2 -> System.out.println("   >>> T2: " + t2));
-    }
-
-    //    @PostConstruct
-    void oneToManyInfinite() {
-        requester.route("thing2stream")
-                .data(new Thing1(UUID.randomUUID().toString(), "RSocket Thing1"))
-                .retrieveFlux(Thing2.class)
-                .take(10)
-                .onBackpressureDrop()
-                .subscribe(t2 -> System.out.println("   >>> T2: " + t2));
-    }
-
-    @PostConstruct
-    void manyToMany() {
-        requester.route("pingpong")
-                .data(Flux.interval(Duration.ofSeconds(1))
-                        .map(l -> new Thing1(UUID.randomUUID().toString(), "RSocket Thing1")))
-                .retrieveFlux(Thing2.class)
-                .subscribe(t2 -> System.out.println("   >>> T2: " + t2));
-    }
-}
-
-//@Controller
-//class RSController {
-class RSClientHandler {
-    @MessageMapping("clientcall")
-    public Flux<Thing1> helloAreYouThere() { //(String status) {
-        System.out.println(" >> Hello? Is it me you're looking for?");
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(l -> new Thing1(UUID.randomUUID().toString(),
-                        "Thing1 coming to you from thing1-service!"));
-    }
-}
-
-class RSClient {
-    private RSocketRequester rsocketRequester;
-    private RSocketRequester.Builder rsocketRequesterBuilder;
-    private RSocketStrategies rsocketStrategies;
-    private String clientId = UUID.randomUUID().toString();
-
-    public RSClient(RSocketRequester.Builder builder,
-                    RSocketStrategies strategies) {
-        this.rsocketRequesterBuilder = builder;
-        this.rsocketStrategies = strategies;
-
-        SocketAcceptor acceptor = RSocketMessageHandler.responder(strategies, new RSClientHandler());
-
-        this.rsocketRequester = rsocketRequesterBuilder
-                .setupRoute("rsclient")
-                .setupData(clientId)
-                .rsocketStrategies(strategies)
-                .rsocketConnector(connector -> connector.acceptor(acceptor))
-                .connectTcp("localhost", 8081)
-                .block();
-
-        this.rsocketRequester.rsocket()
-                .onClose()
-                .doOnError(error -> System.out.println("Connection CLOSED"))
-                .doFinally(consumer -> System.out.println("Client DISCONNECTED"))
-                .subscribe();
-    }
-
-    @PreDestroy
-    public void cleanUp() {
-        this.rsocketRequester.rsocket().dispose();
+@Configuration
+class Thing1Config {
+    @Bean
+    Consumer<Flux<Thing2>> receiveThing2s() {
+        return thing2Flux -> thing2Flux.subscribe(System.out::println);
     }
 }
 
--- thing2-service/src/main/java/com/thehecklers/thing2service/Thing2ServiceApplication.java
+++ thing2-service/src/main/java/com/thehecklers/thing2service/Thing2ServiceApplication.java
@@ -5,22 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Thing2ServiceApplication {
@@ -31,10 +30,20 @@ public class Thing2ServiceApplication {
 
 }
 
+@Configuration
+class Thing2Config {
+    @Bean
+    Supplier<Flux<Thing2>> sendThings() {
+        return () -> Flux.interval(Duration.ofSeconds(1))
+                .map(l -> new Thing2(UUID.randomUUID().toString(),
+                        "Thing 2 from thing2-service"))
+                .log();
+    }
+}
+
 @Controller
 @RequestMapping("/thingtwothings")
 class ThingController {
-    private final List<RSocketRequester> rsClients = new ArrayList<>();
     private Random rnd = new Random();
     private List<String> names = List.of("Alpha", "Bravo", "Charlie", "Delta", "Echo");
 
@@ -60,59 +69,6 @@ class ThingController {
         return Flux.interval(Duration.ofSeconds(1))
                 .map(l -> new Thing2(UUID.randomUUID().toString(),
                         names.get(rnd.nextInt(names.size()))))
-                .log();
-    }
-
-    @ConnectMapping("rsclient")
-    void connectClient(RSocketRequester requester, @Payload String clientId) {
-        requester.rsocket()
-                .onClose()
-                .doFirst(() -> {
-                    System.out.println("Client connected: " + clientId);
-                    rsClients.add(requester);
-                })
-                .doOnError(err -> System.out.println("Channel to client closed: " + clientId))
-                .doFinally(consumer -> {
-                    rsClients.remove(requester);
-                    System.out.println("Client disconnnected: " + clientId);
-                })
-                .subscribe();
-
-        requester.route("clientcall")
-                .data("Greetings Thing1")
-                .retrieveFlux(Thing1.class)
-//                .doOnNext(t1 -> System.out.println("  >> Received T1: " + t1))
-                .subscribe(System.out::println);
-    }
-
-    @MessageMapping("thing2")
-    Mono<Thing2> getThingRS() { // No param? 1:1
-        return Mono.just(new Thing2(UUID.randomUUID().toString(),
-                "Thing 2 from thing2-service"));
-    }
-
-    @MessageMapping("afewthing2s")
-    Flux<Thing2> getThingsRS() { // No param? 1:N
-        return Flux.fromStream(
-                names.stream()
-                        .map(name -> new Thing2(UUID.randomUUID().toString(), name)));
-
-    }
-
-    @MessageMapping(value = "thing2stream")
-    Flux<Thing2> getThingStreamRS() { // No param? 1:N forever
-        return Flux.interval(Duration.ofSeconds(1))
-                .map(l -> new Thing2(UUID.randomUUID().toString(),
-                        names.get(rnd.nextInt(names.size()))))
-                .log();
-    }
-
-    @MessageMapping("pingpong")
-    Flux<Thing2> bidirectionalChannel(Flux<Thing1> things) {
-        return things
-                .doOnNext(thing1 -> System.out.println("  >>> Received: " + thing1))
-                .switchMap(thing1 -> Flux.just(new Thing2(UUID.randomUUID().toString(),
-                        names.get(rnd.nextInt(names.size())))))
                 .log();
     }
 }
